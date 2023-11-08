97f5f412e53c316164f29f1a9eaf379b888c16f0
GregJohnStewart <contact@gjstewart.net>
2023-11-08 14:42:29 -0500

    Fixed issue with messenger not liking the object

--- src/main/java/org/acme/interfaces/message/CalculatorMessenger.java
+++ src/main/java/org/acme/interfaces/message/CalculatorMessenger.java
@@ -1,8 +1,12 @@
 package org.acme.interfaces.message;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.smallrye.common.annotation.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.json.JsonObject;
 import org.acme.service.CalculationRequest;
 import org.acme.service.CalculationResult;
 import org.acme.service.CalculatorService;
@@ -19,13 +23,19 @@ public class CalculatorMessenger {
     @Inject
     CalculatorService calculatorService;
 
+    @Inject
+    ObjectMapper mapper;
+
     public void sendResult(CalculationResult result){
         this.quoteRequestEmitter.send(result);
     }
 
     @Incoming("calculator-requests")
     @Blocking
-    public void process(CalculationRequest request) {
+    public void process(JsonNode requestJson) throws JsonProcessingException {
+        CalculationRequest request = this.mapper.treeToValue(requestJson, CalculationRequest.class);
+
+
         this.calculatorService.calculate(request);
     }
 
